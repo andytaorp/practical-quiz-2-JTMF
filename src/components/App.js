62e951a9944e07521083f711086f3b2bfd634f12
JTMF <23014056@myrp.edu.sg>
2024-11-28 15:50:43 +0800

    1

--- src/components/App.js
+++ src/components/App.js
@@ -7,21 +7,33 @@ function App() {
 
   const handleAddHabit = (habitName) => {
     // TODO: write code to add a new habit here
+    const newHabit = {  
+      id: Date.now(),
+      habit: habitName,
+      completed: false, 
+    };
+    setHabits([...habits, newHabit]);
   };
 
   const handleToggleHabit = (id) => {
     // TODO: write code to toggle a habit's status
+    setHabits(habits.map((habit) =>
+        habit.id === id ? { ...habit, completed: !habit.completed } : habit)
+    );
   };
 
   const handleDeleteHabit = (id) => {
     // TODO: write code to delete a habit
+    setHabits((prevItems) => prevItems.filter(habits => habits.id !== id));
   };
 
   return (
     <div>
       <h1>Habit Tracker</h1>
       {/*TODO: add a form to add a new habit*/}
+      <AddHabitForm handleAddHabit={handleAddHabit}/>
       {/*TODO: add a list of habits*/}
+      <HabitList habits={habits} onToggleHabit={handleToggleHabit} onDeleteHabit={handleDeleteHabit}/>
     </div>
   );
 }
